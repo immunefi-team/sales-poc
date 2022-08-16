bee76f7e0f6fc5cc6e14e918ef6bc596632549a3
Arbaz Hussain <arbazhussain149@gmail.com>
2022-08-16 16:42:54 +0530

    add README#3

--- .history/contracts/IVulnerable_20220816164157.sol
+++
@@ -1,6 +0,0 @@
-interface IVulnerable {
-    function balanceOf(address user) external returns(uint256);
-    function deposit() payable external;
-    function setLockingPeriod(uint256 newPeriod) external;
-    function withdraw(uint256 amount) external;
-}
--- .history/contracts/IVulnerable_20220816164204.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.8.4; 
-
-interface IVulnerable {
-    function balanceOf(address user) external returns(uint256);
-    function deposit() payable external;
-    function setLockingPeriod(uint256 newPeriod) external;
-    function withdraw(uint256 amount) external;
-}
